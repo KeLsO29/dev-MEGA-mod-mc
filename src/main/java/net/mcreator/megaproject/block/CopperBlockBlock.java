9e850552343b360a26c13b09a1aefe082a2d9b80
KeLsO29 <matus121164@gmail.com>
2021-01-21 10:01:15 +0100

    deleted copper sheet

--- src/main/java/net/mcreator/megaproject/block/CopperBlockBlock.java
+++ src/main/java/net/mcreator/megaproject/block/CopperBlockBlock.java
@@ -1,17 +1,30 @@
 
 package net.mcreator.megaproject.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.megaproject.MegaProjectModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @MegaProjectModElements.ModElement.Tag
 public class CopperBlockBlock extends MegaProjectModElements.ModElement {
-
 	@ObjectHolder("mega_project:copper_block")
 	public static final Block block = null;
-
 	public CopperBlockBlock(MegaProjectModElements instance) {
 		super(instance, 24);
-
 	}
 
 	@Override
@@ -20,27 +33,19 @@ public class CopperBlockBlock extends MegaProjectModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).lightValue(0).harvestLevel(2)
-							.harvestTool(ToolType.PICKAXE));
-
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).lightValue(0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE));
 			setRegistryName("copper_block");
 		}
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/megaproject/procedures/IronnodestructureAdditionalGenerationConditionProcedure.java
+++ src/main/java/net/mcreator/megaproject/procedures/IronnodestructureAdditionalGenerationConditionProcedure.java
@@ -1,11 +1,16 @@
 package net.mcreator.megaproject.procedures;
 
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+
+import net.mcreator.megaproject.MegaProjectModElements;
+
+import java.util.Map;
+
 @MegaProjectModElements.ModElement.Tag
 public class IronnodestructureAdditionalGenerationConditionProcedure extends MegaProjectModElements.ModElement {
-
 	public IronnodestructureAdditionalGenerationConditionProcedure(MegaProjectModElements instance) {
 		super(instance, 24);
-
 	}
 
 	public static boolean executeProcedure(Map<String, Object> dependencies) {
@@ -29,17 +34,13 @@ public class IronnodestructureAdditionalGenerationConditionProcedure extends Meg
 				System.err.println("Failed to load dependency world for procedure IronnodestructureAdditionalGenerationCondition!");
 			return false;
 		}
-
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
 		if ((world.canBlockSeeSky(new BlockPos((int) x, (int) y, (int) z)))) {
 			return (true);
 		}
 		return (false);
-
 	}
-
 }
