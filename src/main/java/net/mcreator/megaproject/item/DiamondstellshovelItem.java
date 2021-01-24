ec8494e160277c14a9a719ca4c07d9527faf098b
desperis1 <dusan.zongor@gmail.com>
2021-01-24 16:49:18 +0100

    electric furnanace

---
+++ src/main/java/net/mcreator/megaproject/item/DiamondstellshovelItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.megaproject.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.megaproject.MegaProjectModElements;
+
+@MegaProjectModElements.ModElement.Tag
+public class DiamondstellshovelItem extends MegaProjectModElements.ModElement {
+	@ObjectHolder("mega_project:diamondstellshovel")
+	public static final Item block = null;
+	public DiamondstellshovelItem(MegaProjectModElements instance) {
+		super(instance, 118);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ShovelItem(new IItemTier() {
+			public int getMaxUses() {
+				return 115144;
+			}
+
+			public float getEfficiency() {
+				return 15f;
+			}
+
+			public float getAttackDamage() {
+				return 7f;
+			}
+
+			public int getHarvestLevel() {
+				return 3;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -2f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("diamondstellshovel"));
+	}
+}
---
+++ src/main/java/net/mcreator/megaproject/procedures/ElectricFurnanceBlockClientDisplayRandomTickProcedure.java
@@ -0,0 +1,67 @@
+package net.mcreator.megaproject.procedures;
+
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.particles.ParticleTypes;
+
+import net.mcreator.megaproject.MegaProjectModElements;
+
+import java.util.Map;
+
+@MegaProjectModElements.ModElement.Tag
+public class ElectricFurnanceBlockClientDisplayRandomTickProcedure extends MegaProjectModElements.ModElement {
+	public ElectricFurnanceBlockClientDisplayRandomTickProcedure(MegaProjectModElements instance) {
+		super(instance, 123);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure ElectricFurnanceBlockClientDisplayRandomTick!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure ElectricFurnanceBlockClientDisplayRandomTick!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure ElectricFurnanceBlockClientDisplayRandomTick!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure ElectricFurnanceBlockClientDisplayRandomTick!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (((new Object() {
+			public boolean getValue(BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getBoolean(tag);
+				return false;
+			}
+		}.getValue(new BlockPos((int) x, (int) y, (int) z), "Working")) == (true))) {
+			world.addParticle(ParticleTypes.LAVA, (x + 0.5), y, (z + 0.5), 0, 0.1, 0);
+			if (!world.getWorld().isRemote) {
+				world.playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.furnace.fire_crackle")),
+						SoundCategory.NEUTRAL, (float) 0.3, (float) 0.3);
+			} else {
+				world.getWorld().playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.furnace.fire_crackle")),
+						SoundCategory.NEUTRAL, (float) 0.3, (float) 0.3, false);
+			}
+		}
+	}
+}
